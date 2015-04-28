fe680e6679fd8c1ae4776e20d4e8471611dde3eb
Alexander Floh <alexander.floh@gmail.com>
2015-04-28 21:08:46 +0200

    added custom 404 page

--- app/Global.scala
+++ app/Global.scala
@@ -1,5 +1,6 @@
 import play.api._
 import play.api.mvc._
+import play.api.mvc.Results._
 import play.api.libs.concurrent.Akka
 import play.api.Play.current
 import akka.actor.Props
@@ -9,19 +10,26 @@ import utils.CleanUp
 import play.api.libs.concurrent.Execution.Implicits._
 import utils.AnalyzeMaster
 import play.filters.gzip.GzipFilter
+import scala.concurrent.Future
 
 object Global extends WithFilters(new GzipFilter()) with GlobalSettings {
   override def onStart(app: Application) {
     val actor = Akka.system.actorOf(Props[CleanUpActor], name = "cleanUpActor")
     def interval = Play.mode match {
-      case Mode.Dev => 1.minutes
+      case Mode.Dev  => 1.minutes
       case Mode.Prod => 3.hours
     }
     Akka.system.scheduler.schedule(5.seconds, interval, actor, CleanUp)
-    
+
     val analyzeMaster = Akka.system.actorOf(Props[AnalyzeMaster], name = "analyzeMaster")
   }
 
   override def onStop(app: Application) {
+  }
+
+  override def onHandlerNotFound(request: RequestHeader) = {
+    Future.successful(NotFound(
+      views.html.pageNotFound(request.path)
+    ))
   }
 }
