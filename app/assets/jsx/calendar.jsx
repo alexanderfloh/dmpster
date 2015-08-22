ba2e3f4c16ddc9fa8ea1979f372021e714e38f01
Alexander Floh <alexander.floh@gmail.com>
2015-08-22 22:44:36 +0200

    improved rendering performance by delaying calendar rendering

--- app/assets/jsx/calendar.jsx
+++ app/assets/jsx/calendar.jsx
@@ -10,37 +10,39 @@ define(['react', 'd3', 'calHeatmap'],
     componentDidMount: function() {
       if(this.props.bucketId) {
         var that = this;
-        var dt = new Date();
-        var currentMonth = dt.getMonth();
-        var month = currentMonth;
-        var futureDaysInMonth = ['now'];
-        var daysOffset = 1;
-        dt.setDate(dt.getDate() + daysOffset++);
-
-        while(month === currentMonth) {
-          futureDaysInMonth.push(dt);
-          dt = new Date();
+        window.setTimeout(function() {
+          var dt = new Date();
+          var currentMonth = dt.getMonth();
+          var month = currentMonth;
+          var futureDaysInMonth = ['now'];
+          var daysOffset = 1;
           dt.setDate(dt.getDate() + daysOffset++);
-          month = dt.getMonth();
-        }
+
+          while(month === currentMonth) {
+            futureDaysInMonth.push(dt);
+            dt = new Date();
+            dt.setDate(dt.getDate() + daysOffset++);
+            month = dt.getMonth();
+          }
 
 
-        var cal = new CalHeatMap();
-        cal.init({
-          itemSelector: '#cal-heatmap' + that.props.bucketId,
-          domain: 'month',
-          range : 3,
-          start: new Date().setMonth(new Date().getMonth() - 2),
-          displayLegend: false,
-          legend: [1, 5, 10, 15],
-          highlight: futureDaysInMonth
-        });
-        $.ajax({
-          url: '/dmpster/bucket/' + that.props.bucketId + '/hits.json',
+          var cal = new CalHeatMap();
+          cal.init({
+            itemSelector: '#cal-heatmap' + that.props.bucketId,
+            domain: 'month',
+            range : 3,
+            start: new Date().setMonth(new Date().getMonth() - 2),
+            displayLegend: false,
+            legend: [1, 5, 10, 15],
+            highlight: futureDaysInMonth
+          });
+          $.ajax({
+            url: '/dmpster/bucket/' + that.props.bucketId + '/hits.json',
 
-        }).done(function(data){
-          cal.update(data);
-        });
+          }).done(function(data){
+            cal.update(data);
+          });
+        }, 1000);
       }
     },
 
